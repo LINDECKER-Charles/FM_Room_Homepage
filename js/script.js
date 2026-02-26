1ebbb280f1210069b9fcaa02955c482e95382f26
LINDECKER Charles <92403010+LINDECKER-Charles@users.noreply.github.com>
2026-02-26 16:38:36 +0100

    feat(slider) : add slider with first section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/* Burger Menu */
+
 const burgerOpenBtn = document.getElementById('open');
 const burgerCloseBtn = document.getElementById('close');
 const burger = document.getElementById('burger');
@@ -13,3 +15,30 @@ burger.addEventListener('click', () => {
 
     mainNav.classList.toggle('translate-x-full');
 });
+
+/* Slider */
+
+const mainSect = document.getElementById('main-section');
+const arrowLeft = document.getElementById('arrow-left');
+const arrowRight = document.getElementById('arrow-right');
+
+function slide(index) {
+  mainSect.style.transform = `translateX(-${index * 100}vw)`
+}
+
+let currentIndex = 0;
+
+arrowLeft.addEventListener('click', () => {
+    if(currentIndex > 0){
+        currentIndex--;
+        slide(currentIndex);
+    }
+});
+
+arrowRight.addEventListener('click', () => {
+    if(currentIndex < 2){
+        currentIndex++;
+        slide(currentIndex);
+    }
+});
+
